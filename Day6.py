696390eb602f64a9be0a2b0539a3863e2c016224
HeSer <B@nd@r1359>
2023-01-08 10:23:42 -0500

    update v1

---
+++ Day6.py
@@ -0,0 +1,29 @@
+# functions
+
+#%%
+
+print("Today, will be very important day")
+
+#%%
+def my_function():
+    print("hello")
+    print("buy")
+
+my_function()
+
+#%%
+max_run = 3
+while max_run > 0:
+    print("Hello")
+    max_run -= 1
+    
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+#%%
+
